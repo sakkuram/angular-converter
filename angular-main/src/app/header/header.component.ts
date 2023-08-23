c04f290c645968d6fd7b0558f05b5b22f3093b77
sakkuram <114509752+sakkuram@users.noreply.github.com>
2023-08-23 14:36:18 +0300

    Update header.component.ts

--- angular-main/src/app/header/header.component.ts
+++ angular-main/src/app/header/header.component.ts
@@ -13,7 +13,7 @@ export class HeaderComponent implements OnInit {
 
   response: any;
   res: any;
-  // gets actual data about currency from API and then returns it  | USD
+  // gets actual data about currency from API and then rturns it  | USD
   getUsd(base:string) {
     this.http.get(`https://cdn.moneyconvert.net/api/latest.json${base}`)
     .subscribe((response)=>{
