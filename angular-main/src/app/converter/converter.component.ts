991a832899d4df4dc438a8de864885c337ee3366
[den] <114509752+sakkuram@users.noreply.github.com>
2023-11-10 18:34:43 +0200

    Update converter.component.ts

--- angular-main/src/app/converter/converter.component.ts
+++ angular-main/src/app/converter/converter.component.ts
@@ -18,7 +18,7 @@ export class ConverterComponent implements OnInit {
   rates!: {[key: string]: number}
   
   convert(): number{
-    return this.amount * this.rates[this.to];123
+    return this.amount * this.rates[this.to];1234
   }
 
   loadRates(){
