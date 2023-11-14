68eda208c5316a32657d80a7cf593e7235b68943
[den] <114509752+sakkuram@users.noreply.github.com>
2023-11-14 21:04:40 +0200

    Update main.ts

--- angular-main/src/main.ts
+++ angular-main/src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';12345
+import { environment } from './environments/environment';123456
 
 if (environment.production) {
   enableProdMode();
