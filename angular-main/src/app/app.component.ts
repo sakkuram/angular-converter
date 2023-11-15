09e1d85073011137ae0c4a164e37367bb23f9a8e
[den] <114509752+sakkuram@users.noreply.github.com>
2023-11-15 21:06:57 +0200

    Update app.component.ts

--- angular-main/src/app/app.component.ts
+++ angular-main/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-})
+})1
 export class AppComponent {
   title = 'my-app';
 
