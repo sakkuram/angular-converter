4775fddc476e63162e756eb0c8683fa14c66368b
[den] <114509752+sakkuram@users.noreply.github.com>
2023-11-19 03:36:03 +0200

    Update app-routing.module.ts

--- angular-main/src/app/app-routing.module.ts
+++ angular-main/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
+2import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConverterComponent } from './converter/converter.component';
 
 const routes: Routes = [
-  {path:'', component: ConverterComponent},123456789012345678901
+  {path:'', component: ConverterComponent},1234567890123456789012
 
 ];
 
